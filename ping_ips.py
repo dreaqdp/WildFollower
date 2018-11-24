7fbd72cd9c05482ef14d0ff926387396433054cd
Fèlix Arribas Pardo <felixarpa@gmail.com>
2018-11-24 18:09:07 +0100

    improve ping command

--- ping_ips.py
+++ ping_ips.py
@@ -1,9 +1,23 @@
 import os
 
-for i in range(1, 254):
+ips = []
+
+for i in range(0, 256):
     ip = "169.254.247.{}".format(i)
-    response = os.system("ping -c 1 {}".format(ip))
+    command = "ping -c 2 -W 1 -i 0.5 {}".format(ip)
+    print(command)
+    response = os.system(command)
     if response == 0:
+        ips.append(ip)
         print('{} is up!'.format(ip))
     # else:
         # print('{} is down!'.format(ip))
+
+print()
+print()
+print()
+print()
+print()
+print()
+
+[ print(ip) for ip in ips ]
